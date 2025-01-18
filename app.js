19be96146acc6b4488950229fee8b5b47d5e1929
DanielCrema <danielborgescrema@gmail.com>
2025-01-18 18:12:50 -0300

    desenvolver modal de erros

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Lógica para gerenciar o botão do modal
+const backdrop = document.getElementById('backdrop');
+const containerWarning = document.getElementById('containerWarning');
+const warningExitButton = document.getElementById('warningExitButton');
+
+warningExitButton.addEventListener('click', () => {
+    backdrop.style.display = 'none';
+    containerWarning.style.display = 'none';
+});
