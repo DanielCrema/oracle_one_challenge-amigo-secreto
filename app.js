27c34347c07e1a63db39f4658af327a266c30060
DanielCrema <danielborgescrema@gmail.com>
2025-01-18 19:13:49 -0300

    criar função de apresentar o modal

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-// Lógica para gerenciar o botão do modal
+// Capturando os elementos do modal e do input no HTML
 const backdrop = document.getElementById('backdrop');
-const containerWarning = document.getElementById('containerWarning');
+const containerModal = document.getElementById('containerModal');
+const containerWarningMessage = document.getElementById('warningMessage');
+const warningTitle = document.getElementById('warningTitle');
+const warningSubtitleDiv = document.getElementById('missingField');
 const warningExitButton = document.getElementById('warningExitButton');
+const inputFriendName = document.getElementById('amigo');
 
+// Lógica para gerenciar o botão de fechar o modal
 warningExitButton.addEventListener('click', () => {
     backdrop.style.display = 'none';
-    containerWarning.style.display = 'none';
+    containerModal.style.display = 'none';
+    inputFriendName.value = '';
 });
+
+// Função para exibir o modal
+function displayModal(displayCase, alertMessage) {
+    warningTitle.innerHTML = alertMessage.title;
+    if (displayCase == 'noSubtitle') {
+        warningSubtitleDiv.style.display = 'none';
+        containerWarningMessage.style.width = '450px';
+        containerWarningMessage.style.minHeight = '150px';
+        containerWarningMessage.style.gap = '0px';
+    } else if (displayCase == 'withSubtitle') {
+        warningSubtitleDiv.innerHTML = `<p>${alertMessage.subtitle}</p>`;
+        warningSubtitleDiv.style.display = 'flex';
+        containerWarningMessage.style.width = '500px';
+        containerWarningMessage.style.minHeight = '200px';
+        containerWarningMessage.style.gap = '30px';
+    }
+    backdrop.style.display = 'block';
+    containerModal.style.display = 'flex';
+}
